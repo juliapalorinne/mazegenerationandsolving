16b80fdf46d102455b9ff96890e65288c927c166
juliapalorinne <julia.palorinne@helsinki.fi>
2021-08-03 18:20:26 +0300

    more tests for depth-first search

--- mazeGenerationAndSolving/src/test/java/maze/DepthfirstSearchTest.java
+++ mazeGenerationAndSolving/src/test/java/maze/DepthfirstSearchTest.java
@@ -33,6 +33,24 @@ public class DepthfirstSearchTest {
         }
     }
     
+    
+    @Test
+    public void moveDownWorksCorrectly() {
+        assertTrue(search.moveDown() == true);        
+        search.maze.getCell(0, 2).visit();
+        assertTrue(search.moveDown() == false);        
+    }
+    
+    @Test
+    public void moveUpWorksCorrectly() {
+        assertTrue(search.moveUp() == false);
+        search.y = 5;
+        assertTrue(search.moveUp() == true);
+        search.maze.getCell(0, 3).visit();
+        assertTrue(search.moveUp() == false);        
+    }
+    
+    
     @Test
     public void chooseDirectionStaysWithinTheMaze() {
        for (int i = 0; i < 5; i++) {
@@ -54,5 +72,46 @@ public class DepthfirstSearchTest {
             c.visit();
         }
     }
+    
+    @Test
+    public void checkIfAllVisitedReturnsCorrectAnswer() {
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(1, 0).visit();
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(0, 1).visit();
+        assertTrue(search.checkIfAllVisited() == true);
+        
+        search.x = 2;
+        search.y = 2;
+        
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(1, 2).visit();
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(2, 1).visit();
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(3, 2).visit();
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(2, 3).visit();
+        assertTrue(search.checkIfAllVisited() == true);  
+        
+        search.x = 49;
+        search.y = 49;
+        
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(49, 48).visit();
+        assertTrue(search.checkIfAllVisited() == false);
+        
+        search.maze.getCell(48, 49).visit();
+        assertTrue(search.checkIfAllVisited() == true);
+    }
+    
+    
     
 }
